1499dd58a08241e3f9111b6375c9173b0de40a79
M4CH1MA <jonathandallavecchia23@gmail.com>
2024-03-18 21:15:54 -0300

    model

--- helpdesk/models.py
+++ helpdesk/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Chamado(models.Model):
+    title = models.CharField(max_length=60)
+    description = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now())
