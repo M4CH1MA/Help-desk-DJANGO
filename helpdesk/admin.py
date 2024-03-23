9043f629e4dfb9fd6f1bd562a6d311a2a8bb942a
M4CH1MA <jonathandallavecchia23@gmail.com>
2024-03-22 22:31:08 -0300

    model Category

--- helpdesk/admin.py
+++ helpdesk/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from helpdesk import models
 
 # Register your models here.
+
+@admin.register(models.Call)
+class HelpdeskAdmin(admin.ModelAdmin):
+    list_display = 'id', 'title',
+    ordering = '-id',
+    list_per_page = 10
+    #list_display_links = 'title'
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = 'id',
+
--- helpdesk/models.py
+++ helpdesk/models.py
@@ -2,7 +2,25 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
-class Chamado(models.Model):
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_plural_name = 'Categories'
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+
+class Call(models.Model):
     title = models.CharField(max_length=60)
     description = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now())
+    created_date = models.DateTimeField(default=timezone.now)
+    show = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+    
