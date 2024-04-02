b10996c0429a7b8a5f511ff9c8aab242ffef0c72
M4CH1MA <jonathandallavecchia23@gmail.com>
2024-04-01 21:21:52 -0300

    pagination

--- helpdesk/views/helpdesk_views.py
+++ helpdesk/views/helpdesk_views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from helpdesk.models import Call
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
     calls = Call.objects.filter(show=True).order_by('-id')
+
+    paginator = Paginator(calls, 3)  
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'calls':calls,
+        'page_obj':page_obj,
     }
     return render(request, 'helpdesk/index.html', context)
 
@@ -17,9 +24,16 @@ def search(request):
         return redirect('helpdesk:index')
 
     calls = Call.objects.filter(show=True).filter(title__contains=search_value).order_by('-id')
+
+    paginator = Paginator(calls, 3)  
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'calls':calls,
-        'search_value': search_value
+        'calls':page_obj,
+        'search_value': search_value,
+        'page_obj': page_obj,
     }
     return render(request, 'helpdesk/index.html', context)
 
